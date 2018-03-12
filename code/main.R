32bf345888007818df5be2b566e0f7426b8b3341
jdyen <jdl.yen@gmail.com>
2018-03-12 16:38:21 +1100

    add comments to main script

--- code/main.R
+++ code/main.R
@@ -27,15 +27,19 @@ growth_data <- read.csv("./data/growth_data.csv")
 mod_stage <- estimate_mpm(pop_samp = pop_data,
                           site = rep(1, length(pop_data)),
                           growdat = growth_data,
-                          mat_type = "stage",
-                          dens_depend = "bh",
+                          mat_type = "stage",  # fit a stage-structured model
+                          dens_depend = "bh",  # use Beverton-Holt density dependence
                           greta_settings = list(nsamples = 1000,
                                                 nwarmup = 1000,
                                                 inits = "random"))
 
-# summarise fitted model
+# plot observed and fitted population abundance
 plot_summed_abund(mod_stage)
+
+# plot matrix of estimated vital rates
 plot_matrix(mod_stage)
+
+# plot projected species's abundances
 plot_projections(mod_stage)
 
 # fit a model with age-structured transitions
@@ -43,7 +47,7 @@ mod_age <- estimate_mpm(pop_samp = pop_data,
                         site = rep(1, length(pop_data)),
                         growdat = growth_data,
                         mat_type = "age",
-                        dens_depend = "none",
+                        dens_depend = "none",  # do not include density dependence
                         greta_settings = list(nsamples = 1000,
                                               nwarmup = 1000,
                                               inits = "random"))
@@ -53,7 +57,7 @@ mod_unstruc <- estimate_mpm(pop_samp = pop_data,
                             site = rep(1, length(pop_data)), 
                             growdat = growth_data,
                             mat_type = "unstructured",
-                            dens_depend = "none",
+                            dens_depend = "ricker",  # use Ricker density dependence
                             greta_settings = list(nsamples = 1000,
                                                   nwarmup = 1000,
                                                   inits = "random"))
